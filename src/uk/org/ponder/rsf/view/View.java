0144356decf473456cb8c02604c4bc25f084b543
amb26 <amb26@localhost>
2004-11-12 20:45:35 +0000

    Weekend checkin

--- src/uk/org/ponder/rsf/view/View.java
+++ src/uk/org/ponder/rsf/view/View.java
@@ -2,32 +2,24 @@ package uk.org.ponder.jsfutil;
 
 import javax.faces.component.UIViewRoot;
 
-
 /**
- * This interface must be implemented by classes representing a
- * view for the ClassViewHandler.
- *
+ * This interface must be implemented by classes representing a view for the
+ * ClassViewHandler.
+ * 
  * @author Hans Bergsten, Gefion Software <hans@gefionsoftware.com>
  * @version 1.0
  */
 public abstract class View {
-  /** The UIViewRoot object returned from createView will be given a
-   * String attribute with this name representing the title required for
-   * the specific view rendered.
+  /**
+   * The UIViewRoot object returned from createView will be given a String
+   * attribute with this name representing the title required for the specific
+   * view rendered.
    */
-  public static final String VIEW_NAME = "View name";
-    public abstract UIViewRoot createView(ViewParameters origviewparams);
+  public static final String VIEW_NAME_ATTRIBUTE = "View name";
 
-    /**
-     * @param viewid
-     */
+  public abstract UIViewRoot createView(ViewParameters origviewparams);
 
-    String viewid;
-    public void setViewID(String viewid) {
-      this.viewid = viewid;
-    }
-    public String getViewID() {
-      return viewid;
-    }
-    public abstract ViewParameters getDefaultParameters();
+  public abstract String getViewID();
+
+  public abstract ViewParameters getDefaultParameters();
 }
