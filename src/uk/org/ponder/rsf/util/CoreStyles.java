a7b589e3c7d0f90a9f35e2901ffb2620a0d998a5
Antranig Basman <amb26@ponder.org.uk>
2004-11-26 23:50:05 +0000

    Weekend checkin

---
+++ src/uk/org/ponder/rsf/util/CoreStyles.java
@@ -0,0 +1,12 @@
+/*
+ * Created on Nov 24, 2004
+ */
+package uk.org.ponder.jsfutil;
+
+/**
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * 
+ */
+public class CoreStyles {
+  public static final String STYLE_ERROR="error-message-style-class";
+}
