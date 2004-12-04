cb65906e92a78f8ac9af10f768f4f56a30df049f
amb26 <amb26@localhost>
2004-12-04 01:52:16 +0000

    Weekend checkin - moderation virtually working

--- src/uk/org/ponder/rsf/view/View.java
+++ src/uk/org/ponder/rsf/view/View.java
@@ -2,6 +2,8 @@ package uk.org.ponder.jsfutil;
 
 import javax.faces.component.UIViewRoot;
 
+import uk.org.ponder.errorutil.MessageLocator;
+
 /**
  * This interface must be implemented by classes representing a view for the
  * ClassViewHandler.
@@ -17,9 +19,19 @@ public abstract class View {
    */
   public static final String VIEW_NAME_ATTRIBUTE = "View name";
 
-  public abstract UIViewRoot createView(ViewParameters origviewparams);
+  public abstract void createView(UIViewRoot viewroot, ViewParameters origviewparams);
 
   public abstract String getViewID();
 
   public abstract ViewParameters getDefaultParameters();
+  
+  protected MessageLocator messagelocator;
+  
+  public void setMessageLocator(MessageLocator messagelocator) {
+    this.messagelocator = messagelocator;
+  }
+  
+  public MessageLocator getMessageLocator() {
+    return messagelocator;
+  }
 }
