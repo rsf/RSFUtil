0446b62770a1ebaf25d84a19f9350ae80fb6ff3f
amb26 <amb26@localhost>
2005-11-01 17:37:53 +0000

    Test for svn/refactoring support

---
+++ src/uk/org/ponder/rsf/componentprocessor/ComponentProcessor.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Aug 7, 2005
+ */
+package uk.org.ponder.rsf.componentprocessor;
+
+import uk.org.ponder.rsf.components.UIComponent;
+
+/**
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * 
+ */
+public interface ComponentProcessor {
+  public void processComponent(UIComponent toprocess);
+}
