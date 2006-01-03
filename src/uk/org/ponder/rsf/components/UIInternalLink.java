d87c0ae1829ec81396a4d07b0478fde030b5ba07
Antranig Basman <amb26@ponder.org.uk>
2006-01-03 16:31:31 +0000

    Documentation updates, SWF DTD added

--- src/uk/org/ponder/rsf/components/UIInternalLink.java
+++ src/uk/org/ponder/rsf/components/UIInternalLink.java
@@ -5,6 +5,16 @@ package uk.org.ponder.rsf.components;
 
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 
+/** A specialisation of a {@link UILink} which specifies a navigation target which is part
+ * of the same RSF application. In this case, specification of the target can be done in the
+ * abstract, by way of a ViewParameters object, allowing "links to self" to be transparently
+ * resolved correctly in whatever the hosting environment is for the current request (servlet,
+ * portlet, etc.). The <code>target</code> field will be filled in by a fixup prior to 
+ * rendering.
+ * @author Antranig Basman (amb26@ponder.org.uk)
+ *
+ */
+
 public class UIInternalLink extends UILink {
   public static UIInternalLink make(UIContainer parent, String ID, String text, 
       ViewParameters viewparams) {
