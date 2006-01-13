bbbe8f4de16d8a21a8b6590b674ed25498debabc
amb26 <amb26@localhost>
2006-01-13 12:04:18 +0000

    MonthBean and Select reform

--- src/uk/org/ponder/rsf/components/UIContainer.java
+++ src/uk/org/ponder/rsf/components/UIContainer.java
@@ -9,8 +9,10 @@ public abstract class UIContainer extends UIComponent {
   public abstract ComponentList flattenChildren();
   
   /** A list of key/value pairs which should represent EL bindings
-   * ({@link UIELBinding})
+   * ({@link UIELBinding}). Note that the bindings which are added to a
+   * UICommand's list will ONLY be submitted if the command is the submitting
+   * control.
    */
   //TODO: mapping!!!
-  public ParameterList parameters;
+  public ParameterList parameters = new ParameterList();
 }
