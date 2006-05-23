1653450cbeecab5957e70f648ee4ec2d4306cd6f
Antranig Basman <amb26@ponder.org.uk>
2006-05-23 15:34:17 +0000

    URL rewriting rules reformed - use $context/ for servlet context

--- src/uk/org/ponder/rsf/viewstate/URLRewriter.java
+++ src/uk/org/ponder/rsf/viewstate/URLRewriter.java
@@ -7,31 +7,42 @@ import uk.org.ponder.stringutil.URLUtil;
 import uk.org.ponder.util.Logger;
 
 /**
- * @author Antranig Basman (antranig@caret.cam.ac.uk)
- * 
+ * @author Antranig Basman (antranig@caret.cam.ac.uk) A URL rewriter for
+ *         "resource" URLs (i.e. those which lead outside the RSF system).
  */
 public class URLRewriter {
+  /**
+   * URLs with this prefix will be rewritten to be relative to the "context
+   * resource root" for this application. In a Webapp environment e.g., this
+   * will be the directory one level above WEB-INF/
+   */
+  public static final String CONTEXT_PREFIX = "$context/";
+
   private ViewStateHandler viewstatehandler;
+
   public void setViewStateHandler(ViewStateHandler viewstatehandler) {
     this.viewstatehandler = viewstatehandler;
   }
-  /** Returns null for URLs which do not need rewriting. relpath has leading
+
+  /**
+   * Returns null for URLs which do not need rewriting. relpath has leading
    * slash removed.
    */
   public String rewriteResourceURL(String path, String relpath) {
     String resourceURL = null;
-    if (path.charAt(0) != '/') { 
-      resourceURL = viewstatehandler.getResourceURL(relpath + path);
-    }
-    else if (!URLUtil.isAbsolute(path)){
-      //String resourceURL = origViewHandler.getResourceURL(context, path);
-      resourceURL = viewstatehandler.getResourceURL(
-          path.substring(1));
+    if (!URLUtil.isAbsolute(path) && path.charAt(0) != '/') {
+      if (path.startsWith(CONTEXT_PREFIX)) {
+        resourceURL = viewstatehandler.getResourceURL(path
+            .substring(CONTEXT_PREFIX.length()));
+      }
+      else {
+        resourceURL = viewstatehandler.getResourceURL(relpath + path);
+      }
     }
     if (Logger.log.isDebugEnabled()) {
       Logger.log.debug("getResourceURL returning " + resourceURL + " for path "
-        + path);
+          + path);
     }
-    return resourceURL; 
+    return resourceURL;
   }
 }
