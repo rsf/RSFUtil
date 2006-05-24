666f2db603b2734d76526dddb9ef3cdbc6b65c71
amb26 <amb26@localhost>
2006-05-24 19:46:11 +0000

    Fix for TemplateResolver path requirement

--- src/uk/org/ponder/rsf/templateresolver/SimpleTemplateResolverStrategy.java
+++ src/uk/org/ponder/rsf/templateresolver/SimpleTemplateResolverStrategy.java
@@ -8,8 +8,7 @@ import uk.org.ponder.stringutil.StringList;
 
 /** Resolves an incoming set of ViewParameters onto a list of template path
  * names (minus extension) to be searched for an appropriate template file.
- * <p>The view is assumed to be rendered from a single template from a selection
- * all held within the same base directory.
+ * <p>The view is assumed to be rendered from a single template.
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
  *
  */
@@ -18,10 +17,8 @@ public interface SimpleTemplateResolverStrategy {
   /** @param viewparams The view parameters for the view being rendered.
    * @return A list of resource names (as supplied to some kind of 
    * InputStreamSource) MINUS extension, that will be searched, IN ORDER,
-   * for a view template to be used for the current view. 
+   * for a view template to be used for the current view. These are full
+   * paths relative to the current context root, beginning with a leading slash.
    */
   public StringList resolveTemplatePath(ViewParameters viewparams);
-  
-  /** @return The common base directory that is assumed to hold all templates */
-  public String getBaseDirectory();
 }
