4bfbe1a59ead90dde5819af813f762badefbe8ae
Antranig Basman <amb26@ponder.org.uk>
2006-05-26 01:42:17 +0000

    Docs, and TooltipDecorator

---
+++ src/uk/org/ponder/rsf/renderer/html/decorators/HTMLTooltipDecoratorRenderer.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 26-May-2006
+ */
+package uk.org.ponder.rsf.renderer.html.decorators;
+
+import java.util.Map;
+
+import uk.org.ponder.rsf.components.decorators.UIDecorator;
+import uk.org.ponder.rsf.components.decorators.UITooltipDecorator;
+import uk.org.ponder.rsf.renderer.DecoratorRenderer;
+
+public class HTMLTooltipDecoratorRenderer implements DecoratorRenderer {
+
+  public Class getRenderedType() {
+    return UITooltipDecorator.class;
+  }
+
+  public String getContentTypes() {
+    return "HTML, HTML-FRAGMENT";
+  }
+
+  public void modifyAttributes(UIDecorator decoratoro, String tagname, Map tomodify) {
+    UITooltipDecorator decorator = (UITooltipDecorator) decoratoro;
+    tomodify.put("title", decorator.text);
+  }
+
+}
