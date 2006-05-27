b5e27b17c12d584a94fb503b2175f7cc9dd0552a
amb26 <amb26@localhost>
2006-05-27 17:16:52 +0000

    Commit moving to 0.6.4-dev

--- src/uk/org/ponder/rsf/renderer/html/URLRewriteSCR.java
+++ src/uk/org/ponder/rsf/renderer/html/URLRewriteSCR.java
@@ -17,7 +17,7 @@ import uk.org.ponder.xml.XMLUtil;
 import uk.org.ponder.xml.XMLWriter;
 
 public class URLRewriteSCR implements StaticComponentRenderer {
-  public static final String NAME = "rewrite-url"; 
+  public static final String NAME = "rewrite-url";
   private URLRewriter rewriter;
   private String relpath;
 
@@ -28,36 +28,52 @@ public class URLRewriteSCR implements StaticComponentRenderer {
   public void setURLRewriter(URLRewriter resolver) {
     this.rewriter = resolver;
   }
-  
+
   public void setViewTemplate(ViewTemplate template) {
     this.relpath = template.getRelativePath();
   }
-  
+
   public String resolveURL(String toresolve) {
     return rewriter.rewriteResourceURL(toresolve, relpath);
   }
-  
-  // returns null if no change required.
-  public HashMap getResolvedURLMap(HashMap attrs, String name) {
-    if (rewriter == null)
-      return null;
+
+  /**
+   * @param attrs
+   *          A non-modifiable map of the attributes that may need to be
+   *          filtered.
+   * @param name
+   *          The name of an attribute whose value is a URL requiring rewriting.
+   * @param cloned
+   *          Either a modifiable map of the attributes, or <code>null</code>
+   *          if none yet exists.
+   * @return Either <code>null</code> if no modification was performed, or a
+   *         modifiable attribute map.
+   */
+  public HashMap getResolvedURLMap(HashMap attrs, String name, HashMap cloned) {
     String toresolve = (String) attrs.get(name);
+    if (toresolve == null || rewriter == null) return cloned;
     String resolved = resolveURL(toresolve);
     if (resolved == null) {
-      return null;
+      return cloned;
     }
     else {
-      HashMap attrcopy = (HashMap) attrs.clone();
-      attrcopy.put(name, resolved);
-      return attrcopy;
+      HashMap togo = null;
+      if (cloned == null) {
+        togo = (HashMap) attrs.clone();
+      }
+      else {
+        togo = cloned;
+      }
+      togo.put(name, resolved);
+      return togo;
     }
   }
-  
+
   public static String getLinkAttribute(XMLLump lump) {
-    for (int i = 0; i < HTMLConstants.tagtoURL.length; ++ i) {
-      String[] tags = HTMLConstants.tagtoURL[i]; 
+    for (int i = 0; i < HTMLConstants.tagtoURL.length; ++i) {
+      String[] tags = HTMLConstants.tagtoURL[i];
       String tag = tags[0];
-      for (int j = 1; j < tags.length; ++ j) {
+      for (int j = 1; j < tags.length; ++j) {
         if (lump.textEquals(tags[j]))
           return tag;
       }
@@ -73,7 +89,11 @@ public class URLRewriteSCR implements StaticComponentRenderer {
     XMLLump endopen = lump.open_end;
     String linkattr = getLinkAttribute(lump);
     if (linkattr != null) {
-      newattrs = getResolvedURLMap(lump.attributemap, linkattr);
+      newattrs = getResolvedURLMap(lump.attributemap, linkattr, newattrs);
+    }
+    for (int i = 0; i < HTMLConstants.ubiquitousURL.length; ++i) {
+      newattrs = getResolvedURLMap(lump.attributemap,
+          HTMLConstants.ubiquitousURL[i], newattrs);
     }
     xmlw.writeRaw(lump.buffer, lump.start, lump.length);
     if (newattrs == null) {
