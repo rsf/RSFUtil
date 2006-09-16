824429ee8162158b13b45dcb6f2ee8e7ee9313f1
amb26 <amb26@localhost>
2006-09-16 20:11:23 +0000

    Introducing - UIJointContainer!!

---
+++ src/uk/org/ponder/rsf/components/UIJointContainer.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 16-Sep-2006
+ */
+package uk.org.ponder.rsf.components;
+
+/** A "two-faced" container that expresses a discontinuous "joint" between
+ * two segments of template. The container appears in its containing context
+ * as a BranchContainer with the details contained in the parent - however
+ * the IKAT branch resolution behaviour is "as if" it in fact had the ID
+ * given by its <code>jointID</code> field. This is the core container allowing
+ * modularity of RSF templates and producers ("components").
+ * @author Antranig Basman (amb26@ponder.org.uk)
+ *
+ */
+
+public class UIJointContainer extends UIBranchContainer {
+  public String jointID;
+}
