be3c949898bfe7cce7b34c842d2cf9696ce210fc
amb26 <amb26@localhost>
2006-09-18 17:59:18 +0000

    Refactoring commit

---
+++ src/uk/org/ponder/rsf/renderer/html/HeadCollectingSCR.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 18 Sep 2006
+ */
+package uk.org.ponder.rsf.renderer.html;
+
+import uk.org.ponder.rsf.renderer.CollectingSCR;
+import uk.org.ponder.rsf.renderer.StaticComponentRenderer;
+import uk.org.ponder.rsf.template.XMLLump;
+import uk.org.ponder.xml.XMLWriter;
+
+public class HeadCollectingSCR implements StaticComponentRenderer, CollectingSCR {
+  public static final String NAME = "head-collect";
+  public String getName() {
+    return NAME;
+  }
+
+  public int render(XMLLump lump, XMLWriter xmlw) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public String getCollectingNames() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
