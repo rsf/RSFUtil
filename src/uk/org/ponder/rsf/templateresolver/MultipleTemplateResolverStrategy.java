e1fccedc1910ed267ec230d11fc3fb2aec1092a5
amb26 <amb26@localhost>
2006-09-26 15:39:21 +0000

    Working evolvers! Container.move -> remove

---
+++ src/uk/org/ponder/rsf/templateresolver/MultipleTemplateResolverStrategy.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Sep 26, 2006
+ */
+package uk.org.ponder.rsf.templateresolver;
+
+/** A TemplateResolverStrategy that distinguishes that its returns should
+ * *all* be aggregated into the current template set, rather than the
+ * first that resolves.
+ * @author Antranig Basman (amb26@ponder.org.uk)
+ *
+ */
+
+public interface MultipleTemplateResolverStrategy extends TemplateResolverStrategy {
+  /** Returns <code>true</code> if ALL the strings returns from this resolver
+   * should be used to aggregate templates for this request. If <code>false</code>,
+   * aggregating will stop at the first template which resolves successfully.
+   * @return
+   */
+  boolean isMultiple();
+}
