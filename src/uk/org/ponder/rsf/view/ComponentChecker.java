9a687c83f681cfc8a39a130245deeb1a1a8b8ab7
Antranig Basman <amb26@ponder.org.uk>
2006-10-10 23:12:17 +0000

    Decent comments for ComponentChecker

--- src/uk/org/ponder/rsf/view/ComponentChecker.java
+++ src/uk/org/ponder/rsf/view/ComponentChecker.java
@@ -4,11 +4,16 @@
 package uk.org.ponder.rsf.view;
 
 /**
+ * An interface which may be used during component production, to avoid
+ * generating (expensive) components which will not be rendered by the
+ * current template.
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
  * 
  */
 public interface ComponentChecker {
-  // used during component generation to avoid generating unrendered
-  // components.
+/**
+ * Returns <code>true</code> if the template set for the current view contains
+ * any demand for a component with the supplied ID.
+ */
   public boolean hasComponent(String ID);
 }
