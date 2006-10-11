8b4132efa3a97d11cbd9f34e0da8e4307eec5a94
amb26 <amb26@localhost>
2006-10-11 23:02:24 +0000

    Fix for component as root document element Correct transit of ContentTypeInfo extension to inferred extension

---
+++ src/uk/org/ponder/rsf/templateresolver/ContentTypeAwareTEI.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 11 Oct 2006
+ */
+package uk.org.ponder.rsf.templateresolver;
+
+import uk.org.ponder.rsf.content.ContentTypeInfo;
+import uk.org.ponder.rsf.viewstate.ViewParameters;
+
+public class ContentTypeAwareTEI implements TemplateExtensionInferrer {
+  
+  private ContentTypeInfo contentTypeInfo;
+  
+  public void setContentTypeInfo(ContentTypeInfo contentTypeInfo) {
+    this.contentTypeInfo = contentTypeInfo;
+  }
+
+  public String inferTemplateExtension(ViewParameters viewparams) {
+    return contentTypeInfo.get().fileextension;
+  }
+
+}
