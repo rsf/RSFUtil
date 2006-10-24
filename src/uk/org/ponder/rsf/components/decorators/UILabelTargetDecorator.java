35ae97323bfb3d32e815f59716f7f6213d92026c
amb26 <amb26@localhost>
2006-10-24 14:35:52 +0000

    LabelTargetDecorator

---
+++ src/uk/org/ponder/rsf/components/decorators/UILabelTargetDecorator.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 24 Oct 2006
+ */
+package uk.org.ponder.rsf.components.decorators;
+
+import uk.org.ponder.rsf.components.UIComponent;
+
+/** Specifies that the decorated component is a label which targets another
+ * component. In HTML, this would be represented by a &lt;label for=" tag.
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ */
+
+public class UILabelTargetDecorator extends UIDecorator {
+  public String targetFullID;
+  public UILabelTargetDecorator(UIComponent target) {
+    targetFullID = target.getFullID();
+  }
+}
