52cd7e357f508dd9fcc44158f666d6e94587f845
Antranig Basman <amb26@ponder.org.uk>
2007-02-09 20:30:04 +0000

    UIJointContainer comments

--- src/uk/org/ponder/rsf/components/UIJointContainer.java
+++ src/uk/org/ponder/rsf/components/UIJointContainer.java
@@ -3,27 +3,39 @@
  */
 package uk.org.ponder.rsf.components;
 
-/** A "two-faced" container that expresses a discontinuous "joint" between
- * two segments of template. The container appears in its containing context
- * as a BranchContainer with the details contained in the parent - however
- * the IKAT branch resolution behaviour is "as if" it in fact had the ID
- * given by its <code>jointID</code> field. This is the core container allowing
- * modularity of RSF templates and producers ("components").
+/**
+ * A "two-faced" container that expresses a discontinuous "joint" between two
+ * segments of template. The container appears in its containing context as a
+ * BranchContainer with the details contained in the parent - however the IKAT
+ * branch resolution behaviour is "as if" it in fact had the ID given by its
+ * <code>jointID</code> field. This is the core container allowing modularity
+ * of RSF templates and producers ("components").
+ * 
  * @author Antranig Basman (amb26@ponder.org.uk)
- *
  */
 
 public class UIJointContainer extends UIBranchContainer {
   public String jointID;
-  
-  public UIJointContainer (UIContainer parent, String ID, String jointID) {
+
+  /**
+   * Create a new UIJointContainer and add it to the component tree.
+   * 
+   * @param parent The container to receive this newly created joint container.
+   * @param ID The "client ID" or "source ID" of this container, that is, the
+   *          rsf:id under which the host template and producer refer to the
+   *          component.
+   * @param jointID The "joint ID" or "target ID" of this container, that is,
+   *          the rsf:id under which the implementing or component template
+   *          refer to the component.
+   */
+  public UIJointContainer(UIContainer parent, String ID, String jointID) {
     this.ID = ID;
     this.jointID = jointID;
     parent.addComponent(this);
   }
-  
-  public UIJointContainer (UIContainer parent, String ID) {
+
+  public UIJointContainer(UIContainer parent, String ID) {
     this(parent, ID, null);
   }
-  
+
 }
