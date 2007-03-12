9f4b35178d8f63f21217b31be3d921be900bfbf1
amb26 <amb26@localhost>
2007-03-12 20:00:42 +0000

    Improved constructors for UIFreeAttributeDecorator

--- src/uk/org/ponder/rsf/components/decorators/UIFreeAttributeDecorator.java
+++ src/uk/org/ponder/rsf/components/decorators/UIFreeAttributeDecorator.java
@@ -3,12 +3,14 @@
  */
 package uk.org.ponder.rsf.components.decorators;
 
+import java.util.HashMap;
 import java.util.Map;
 
-/** Allows free interception of *all* XML attributes in the rendered output.
- * This decorator is virtually as dangerous as {@link uk.org.ponder.rsf.components.UIVerbatim}
- * and the dire warnings of sufferings for users at the head of that class 
- * should be repeated.
+/**
+ * Allows free interception of *all* XML attributes in the rendered output. This
+ * decorator is virtually as dangerous as
+ * {@link uk.org.ponder.rsf.components.UIVerbatim} and the dire warnings of
+ * sufferings for users at the head of that class should be repeated.
  * 
  * The attribute list will be applied <i>on top of</i> attributes inherited
  * from the template, but will be <i>overwritten by</i> any component-specific
@@ -17,13 +19,13 @@ import java.util.Map;
  * 
  * Please try to think of technology-neutral abstractions rather than using this
  * class, wherever possible.
+ * 
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
- *
  */
 
 public class UIFreeAttributeDecorator implements UIDecorator {
-  /** A map of String attribute names to String attribute values which will
-   * be used to override any attributes taken from the template.
+  /** A map of String attribute names to String attribute values which will be
+   * used to override any attributes taken from the template.
    */
   public Map attributes;
 
@@ -33,5 +35,20 @@ public class UIFreeAttributeDecorator implements UIDecorator {
   public UIFreeAttributeDecorator(Map attributes) {
     this.attributes = attributes;
   }
+
+  /** Creates a decorator representing the single supplied key/value pair **/
+  public UIFreeAttributeDecorator(String key, String value) {
+    attributes = new HashMap();
+    attributes.put(key, value);
+  }
   
+  /** Supplied with key and value arrays of equal lengths, which will form the
+   * decorator through selecting keys and values pairwise.
+   */
+  public UIFreeAttributeDecorator(String[] keys, String[] values) {
+    attributes = new HashMap();
+    for (int i = 0; i < keys.length; ++ i) {
+      attributes.put(keys[i], values[i]);
+    }
+  }
 }
