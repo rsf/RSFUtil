4a27d8f9641aeb40ab9e490e93a98021e36b9d26
Antranig Basman <amb26@ponder.org.uk>
2007-03-21 18:07:01 +0000

    Updated comments

--- src/uk/org/ponder/rsf/components/UILink.java
+++ src/uk/org/ponder/rsf/components/UILink.java
@@ -5,10 +5,11 @@ package uk.org.ponder.rsf.components;
 
 /**
  * This component peers with a simple navigational link. Operating this link is
- * expected to give rise to an idempotent request. The basic case where the
- * command is rendered using a piece of non-bound text is handled by filling in
- * the "linktext" field. For more complex command contents including bound ones,
- * leave this field as null and add rendering components as childen of the link.
+ * expected to give rise to an idempotent ("render") request. The basic case
+ * where the command is rendered using a piece of non-bound text is handled by
+ * filling in the <code>linktext</code> field. For more complex command
+ * contents including bound ones, leave <code>linktext</code> as null and add
+ * rendering components as childen of the link in the template.
  * 
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
  */
@@ -19,14 +20,20 @@ public class UILink extends UIComponent {
    * onto the context for the current webapp - that is, the directory
    * immediately above WEB-INF.
    * 
-   * <p>
-   * For an InternalLink this will be filled in by a fixup from the
+   * <p/> For an InternalLink this will be filled in by a fixup from the
    * ViewParameters member.
    */
   public UIBoundString target;
 
+  /** A bound String representing any text to be rendered for this control * */
   public UIBoundString linktext;
 
+  /** Construct a navigation link.
+   * @param parent Container to which the link is to be added.
+   * @param ID RSF ID of this link.
+   * @param text Text to be rendered for this link.
+   * @param target @see {@link #target}
+   */
   public static UILink make(UIContainer parent, String ID, String text,
       String target) {
     UIBoundString linktext = null;
@@ -37,6 +44,9 @@ public class UILink extends UIComponent {
     return make(parent, ID, linktext, target);
   }
 
+  /** Construct a navigation link with a bound control (e.g. UIMessage) forming
+   * the link text.
+   */
   public static UILink make(UIContainer parent, String ID,
       UIBoundString linktext, String target) {
     UILink togo = new UILink();
@@ -50,8 +60,11 @@ public class UILink extends UIComponent {
     return togo;
   }
 
+  /** Construct a navigation link which will leave nested markup unchanged
+   * from the template.
+   */
   public static UILink make(UIContainer parent, String ID, String target) {
-    return make(parent, ID, (UIBoundString)null, target);
+    return make(parent, ID, (UIBoundString) null, target);
   }
 
 }
