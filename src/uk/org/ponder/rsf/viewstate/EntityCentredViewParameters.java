222ac0e17cab5cd0ef2aa29e8d934edbc610f852
amb26 <amb26@localhost>
2007-03-27 18:34:02 +0000

    Fix to bring ECVP up to new trunk URL idiom

--- src/uk/org/ponder/rsf/viewstate/EntityCentredViewParameters.java
+++ src/uk/org/ponder/rsf/viewstate/EntityCentredViewParameters.java
@@ -6,7 +6,6 @@ package uk.org.ponder.rsf.viewstate;
 import uk.org.ponder.beanutil.BeanGetter;
 import uk.org.ponder.beanutil.PathUtil;
 import uk.org.ponder.beanutil.entity.EntityID;
-import uk.org.ponder.stringutil.URLUtil;
 
 /**
  * View parameters, which as well as defining the core viewID, are centred 
@@ -19,10 +18,7 @@ public class EntityCentredViewParameters extends ViewParameters {
   public static final String MODE_EDIT = "edit";
   
   public String getParseSpec() {
-   return ViewParameters.BASE_PARSE_SPEC + ", mode";
-  }
-
-  public void clearParams() {
+   return ViewParameters.BASE_PARSE_SPEC + ", @1:entity.ID, mode";
   }
 
   public EntityCentredViewParameters(String viewid, EntityID entity, String mode) {
@@ -30,6 +26,7 @@ public class EntityCentredViewParameters extends ViewParameters {
     this.entity = entity;
     this.mode = mode;
   }
+  
   public EntityCentredViewParameters(String viewid, EntityID entity) {
     this(viewid, entity, null);
   }
@@ -39,22 +36,11 @@ public class EntityCentredViewParameters extends ViewParameters {
   public EntityID entity;
   public String mode;
   
-  public void parsePathInfo(String pathinfo) {
-    String[] pathcomps = URLUtil.splitPathInfo(pathinfo);
-    // remove leading / which is specced to be there
-    viewID = pathcomps[1];
-    entity.ID = pathcomps[2];
-  }
-
-  public String toPathInfo() {
-    return "/" + viewID + "/" + entity.ID;
-  }
-  
   public String getELPath() {
     return PathUtil.buildPath(entity.entityname, entity.ID);
   }
   
-  /** Fetch the references entity from the supplied beangetter */
+  /** Fetch the referenced entity from the supplied beangetter */
   public Object fetch(BeanGetter beangetter) {
     return beangetter.getBean(getELPath());
   }
