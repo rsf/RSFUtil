54b3cdee9252d43e3be7cc300c0943071f6fb7e8
Antranig Basman <amb26@ponder.org.uk>
2007-05-20 15:29:53 +0000

    Moving up to 0.7.1A - ViewParamsInterceptor and EntityBroker updates for SakaiRSF

--- src/uk/org/ponder/rsf/flow/errors/SilentRedirectException.java
+++ src/uk/org/ponder/rsf/flow/errors/SilentRedirectException.java
@@ -3,6 +3,13 @@
  */
 package uk.org.ponder.rsf.flow.errors;
 
+/** Thrown in the event no template can be found for the decoded view - as
+ * well as the default action from the ViewExceptionStrategy, no exception 
+ * will be logged.
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ *
+ */
+
 public class SilentRedirectException extends Exception {
 
 }
---
+++ src/uk/org/ponder/rsf/flow/errors/ViewExceptionStrategyManager.java
@@ -0,0 +1,28 @@
+/*
+ * Created on 18 May 2007
+ */
+package uk.org.ponder.rsf.flow.errors;
+
+import java.util.List;
+
+import uk.org.ponder.rsf.viewstate.ViewParameters;
+
+public class ViewExceptionStrategyManager implements ViewExceptionStrategy {
+  private List strategies;
+
+  public void setStrategyList(List strategies) {
+    this.strategies = strategies;
+  }
+
+  public ViewParameters handleException(Exception e, ViewParameters incoming) {
+    for (int i = 0; i < strategies.size(); ++ i) {
+      ViewExceptionStrategy strategy = (ViewExceptionStrategy) strategies.get(i);
+      ViewParameters result = strategy.handleException(e, incoming);
+      if (result != null) return result;
+    }
+    return null;
+  }
+  
+  
+  
+}
