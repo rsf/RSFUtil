262c0293e12d7bb1afb3c167c90822e2d6a6f3b2
amb26 <amb26@localhost>
2007-06-22 00:07:23 +0000

    AbstractRSACTests now available!! Test cases fixed up and running in the new world

--- test/uk/org/ponder/rsf/test/flow/TestFlowLoader.java
+++ test/uk/org/ponder/rsf/test/flow/TestFlowLoader.java
@@ -3,38 +3,22 @@
  */
 package uk.org.ponder.rsf.test.flow;
 
+import junit.framework.TestCase;
+
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import uk.org.ponder.rsf.components.UIBranchContainer;
 import uk.org.ponder.saxalizer.XMLProvider;
-import uk.org.ponder.streamutil.write.PrintStreamPOS;
 
-public class TestFlowLoader {
-  public static void main(String[] args) {
-//    SAXalizerMappingContext smc = new SAXalizerMappingContext();
-//    ReflectiveCache rcache = new JDKReflectiveCache();
-//    smc.setReflectiveCache(rcache);
-//    RSFMappingLoader rml = new RSFMappingLoader();
-//    SAXalXMLProvider xmlprovider = new SAXalXMLProvider(smc);
-//    
-//    rml.loadStandardMappings(xmlprovider);
-//    XmlFlowFactoryBean xffb = new XmlFlowFactoryBean();
-//    
-    FileSystemXmlApplicationContext fsxac = 
-      new FileSystemXmlApplicationContext("classpath:uk/org/ponder/rsf/test/minicontext.xml");
-//    xffb.setXMLProvider(xmlprovider);
-//    xffb.setLocation("classpath:uk/org/ponder/rsf/test/flow/numberGuess-flow.xml");
-     
-    try {
-//      Flow flow = (Flow) fsxac.getBean("numberGuessFlowDef");
-//      System.out.println(flow.id);
-      UIBranchContainer root = (UIBranchContainer) fsxac.getBean("viewtree");
-      PrintStreamPOS pos = new PrintStreamPOS(System.out);
-      XMLProvider xmlp = (XMLProvider) fsxac.getBean("XMLProvider");
-      System.out.println(xmlp.toString(root));
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
+public class TestFlowLoader extends TestCase {
+  public void testLoadFlow() {
+    FileSystemXmlApplicationContext fsxac = new FileSystemXmlApplicationContext(
+        "classpath:uk/org/ponder/rsf/test/minicontext.xml");
+
+    UIBranchContainer root = (UIBranchContainer) fsxac.getBean("viewtree");
+    XMLProvider xmlp = (XMLProvider) fsxac.getBean("XMLProvider");
+    System.out.println(xmlp.toString(root));
   }
+  
+  
 }
--- test/uk/org/ponder/rsf/test/sitemap/TestSiteMap.java
+++ test/uk/org/ponder/rsf/test/sitemap/TestSiteMap.java
@@ -5,27 +5,30 @@ package uk.org.ponder.rsf.test.sitemap;
 
 import java.util.HashMap;
 
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
+import uk.org.ponder.rsac.test.AbstractRSACTests;
 import uk.org.ponder.rsf.viewstate.BasicViewParametersParser;
 import uk.org.ponder.rsf.viewstate.EntityCentredViewParameters;
 
-public class TestSiteMap {
-  public static void main(String[] args) {
-
-  FileSystemXmlApplicationContext fsxac = 
-    new FileSystemXmlApplicationContext("classpath:uk/org/ponder/rsf/test/minicontext.xml");
-   
-  try {
-    BasicViewParametersParser bvpp = (BasicViewParametersParser) fsxac.getBean("viewParametersParser");
+public class TestSiteMap extends AbstractRSACTests {
+  public void testParseECVP() {
+    BasicViewParametersParser bvpp = (BasicViewParametersParser) applicationContext.getBean("viewParametersParser");
     HashMap attrmap = new HashMap();
     attrmap.put("flowtoken", "ec38f0");
     EntityCentredViewParameters ecvp = (EntityCentredViewParameters) bvpp.parse("/recipe/3652/", attrmap);
     System.out.println("ECVP for entity " + ecvp.entity.ID + " of type " + ecvp.entity.entityname);
   }
-  catch (Exception e) {
-    e.printStackTrace();
+
+  protected String[] getConfigLocations() {
+    return new String[] {
+        "classpath:conf/rsf-config.xml", "classpath:conf/blank-applicationContext.xml",
+        "classpath:uk/org/ponder/rsf/test/sitemap/sitemap-context.xml"
+    };
   }
-}
+
+  public String[] getRequestConfigLocations() {
+    return new String[] {
+        "classpath:conf/rsf-requestscope-config.xml",
+        "classpath:conf/blank-requestContext.xml"};
+  }
 }
 
