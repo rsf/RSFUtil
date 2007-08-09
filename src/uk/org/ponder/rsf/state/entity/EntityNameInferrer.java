c0ce62715d3ad4eb29f834c1bfe5f1c45a2e48d6
amb26 <amb26@localhost>
2007-08-09 15:33:09 +0000

    Cleared contract for EntityNameInferrer

--- src/uk/org/ponder/rsf/state/entity/EntityNameInferrer.java
+++ src/uk/org/ponder/rsf/state/entity/EntityNameInferrer.java
@@ -3,6 +3,15 @@
  */
 package uk.org.ponder.rsf.state.entity;
 
+/** Returns the "name" (in general managing OTP bean, probably of type 
+ * BeanLocator) of an Entity, given its Class.
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ *
+ */ 
+
 public interface EntityNameInferrer {
+  /** Return the name of the entity for the supplied class, or <code>null</code>
+   * if this inferrer does not hold it.
+   */
   public String getEntityName(Class entityclazz);
 }
