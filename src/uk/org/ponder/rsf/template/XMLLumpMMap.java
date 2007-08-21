bda63cee0665d15c1b9480aff3ed365e15dd2679
Antranig Basman <amb26@ponder.org.uk>
2007-08-21 01:10:25 +0000

    Final death of isBranchKey - head collection works once again

--- src/uk/org/ponder/rsf/template/XMLLumpMMap.java
+++ src/uk/org/ponder/rsf/template/XMLLumpMMap.java
@@ -6,8 +6,12 @@ package uk.org.ponder.rsf.template;
 import java.util.HashMap;
 import java.util.Iterator;
 
-import uk.org.ponder.rsf.util.SplitID;
 import uk.org.ponder.stringutil.CharWrap;
+
+/** Maintains a Map of String IDs to XMLLumpList.
+ * 
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ */
 
 public class XMLLumpMMap {
   private HashMap idtolumps = new HashMap(8);
@@ -32,10 +36,6 @@ public class XMLLumpMMap {
     return togo;
   }
 
-  private boolean isBranchKey(String key) {
-    return SplitID.isSplit(key);
-  }
-  
   public XMLLumpList headsForIDEnsure(String ID) {
     XMLLumpList togo = (XMLLumpList) idtolumps.get(ID);
     if (togo == null) {
@@ -71,11 +71,9 @@ public class XMLLumpMMap {
   public void aggregate(XMLLumpMMap toaccrete) {
     for (Iterator it = toaccrete.iterator(); it.hasNext();) {
       String key = (String) it.next();
-      if (toaccrete.isBranchKey(key)) {
-        XMLLumpList list = toaccrete.headsForID(key);
-        for (int i = 0; i < list.size(); ++i) {
-          addLump(key, list.lumpAt(i));
-        }
+      XMLLumpList list = toaccrete.headsForID(key);
+      for (int i = 0; i < list.size(); ++i) {
+        addLump(key, list.lumpAt(i));
       }
     }
   }
