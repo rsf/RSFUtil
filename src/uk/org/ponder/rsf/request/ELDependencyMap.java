30673840ef1ba5efa42b64d6a334066ae2fae3c0
amb26 <amb26@localhost>
2007-09-03 02:44:08 +0000

    Tidied imports

--- src/uk/org/ponder/rsf/request/ELDependencyMap.java
+++ src/uk/org/ponder/rsf/request/ELDependencyMap.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import uk.org.ponder.beanutil.BeanUtil;
 import uk.org.ponder.beanutil.PathUtil;
 import uk.org.ponder.stringutil.StringList;
 
