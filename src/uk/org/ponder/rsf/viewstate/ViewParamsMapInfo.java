f597dc52fecc3f81a3c9a4a77f09ee897e15a2d2
Antranig Basman <amb26@ponder.org.uk>
2007-09-10 15:40:25 +0000

    Refactoring commit - created viewstate.support Reorganising ViewParamsCodec

---
+++ src/uk/org/ponder/rsf/viewstate/ViewParamsMapInfo.java
@@ -0,0 +1,12 @@
+/*
+ * Created on 10 Sep 2007
+ */
+package uk.org.ponder.rsf.viewstate;
+
+public interface ViewParamsMapInfo {
+
+  public String pathToAttribute(String path);
+
+  public String attributeToPath(String attribute);
+
+}
