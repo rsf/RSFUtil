967b7d31a8a6ddd76f0e7b137148d08ef514f599
amb26 <amb26@localhost>
2008-01-09 15:44:40 +0000

    Solidifying work on test cycle framework

---
+++ src/uk/org/ponder/rsf/bare/BareRootHandlerBean.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 9 Jan 2008
+ */
+package uk.org.ponder.rsf.bare;
+
+import uk.org.ponder.rsf.processor.RootHandlerBeanBase;
+import uk.org.ponder.rsf.viewstate.AnyViewParameters;
+import uk.org.ponder.streamutil.write.PrintOutputStream;
+
+public class BareRootHandlerBean extends RootHandlerBeanBase {
+
+  public void issueRedirect(AnyViewParameters viewparamso, PrintOutputStream pos) {
+    
+  }
+
+  public PrintOutputStream setupResponseWriter() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ src/uk/org/ponder/rsf/bare/junit/PlainRSFTests.java
@@ -0,0 +1,50 @@
+/*
+ * Created on 22 Jun 2007
+ */
+package uk.org.ponder.rsf.bare.junit;
+
+import uk.org.ponder.beanutil.WriteableBeanLocator;
+import uk.org.ponder.rsac.test.AbstractRSACTests;
+import uk.org.ponder.rsf.bare.RequestLauncher;
+
+public class PlainRSFTests extends AbstractRSACTests {
+
+  protected String[] getConfigLocations() {
+    return new String[] {
+        "classpath:conf/rsf-config.xml", "classpath:conf/blank-applicationContext.xml",
+        "classpath:conf/test-applicationContext.xml"
+    };
+  }
+
+  protected RequestLauncher requestLauncher;
+  
+  private RequestLauncher allocateRequestLauncher() {
+    return new RequestLauncher(applicationContext, getRSACBeanLocator(), isSingleShot());
+  }
+  
+  public RequestLauncher getRequestLauncher() {
+    if (isSingleShot()) {
+      return requestLauncher;
+    }
+    else {
+      return allocateRequestLauncher();
+    }
+  }
+  
+  public String[] getRequestConfigLocations() {
+    return new String[] {
+        "classpath:conf/rsf-requestscope-config.xml",
+        "classpath:conf/blank-requestContext.xml",
+        "classpath:conf/test-requestContext.xml"};
+  }
+
+  protected void onSetUp() throws Exception {
+    super.onSetUp();
+    WriteableBeanLocator wbl = getRSACBeanLocator().getDeadBeanLocator();
+    if (isSingleShot()) {
+      requestLauncher = allocateRequestLauncher();
+      wbl.set("earlyRequestParser", requestLauncher);
+    }
+  }
+  
+}
