2f0c2aa10faecfc1a42d1cc46ea11a23e6f508ca
amb26 <amb26@localhost>
2008-02-29 19:29:04 +0000

    Fix for RSF-60, RSF-43, RSF-67, coordinated correction to DARApplier for type inference and primitive arrays

---
+++ test/uk/org/ponder/rsf/test/opacity/OpaqueBL.java
@@ -0,0 +1,38 @@
+/*
+ * Created on 8 Jan 2008
+ */
+package uk.org.ponder.rsf.test.opacity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import uk.org.ponder.beanutil.WriteableBeanLocator;
+
+public class OpaqueBL implements WriteableBeanLocator {
+  private Map map = new HashMap();
+
+  public OpaqueBL() {
+    map.put("string", "string");
+    map.put("stringarray", new String[] {"string1", "string2"});
+  }
+  
+  public Object locateBean(String name) {
+    return map.get(name);
+  }
+
+  public boolean remove(String beanname) {
+    return false;
+  }
+
+  public void set(String beanname, Object toset) {
+    if (toset instanceof String[]) {
+      String[] setarr = (String[]) toset;
+      for (int i = 0; i < setarr.length; ++ i) {
+        if (setarr[i] == null) {
+          throw new IllegalArgumentException("Array containing null elements has been applied");
+        }
+      }
+    }
+    map.put(beanname, toset);
+  }
+}
