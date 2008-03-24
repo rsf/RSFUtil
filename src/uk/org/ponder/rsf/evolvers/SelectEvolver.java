ed65809a3a61673af7d219606486266e82fb2822
amb26 <amb26@localhost>
2008-03-24 17:11:29 +0000

    Docs for SelectEvolver

--- src/uk/org/ponder/rsf/evolvers/SelectEvolver.java
+++ src/uk/org/ponder/rsf/evolvers/SelectEvolver.java
@@ -6,6 +6,15 @@ package uk.org.ponder.rsf.evolvers;
 import uk.org.ponder.rsf.components.UIJointContainer;
 import uk.org.ponder.rsf.components.UISelect;
 
+/** The interface to a family of evolvers for selection controls, with the same
+ * binding structure as UISelect. 
+ * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ *
+ */
+
 public interface SelectEvolver {
+  /** Evolve the supplied seed into a branch with the same ID and parent, 
+   * containing a component subtree implementing the "widget"/component.
+   */
   public UIJointContainer evolveSelect(UISelect toevolve);
 }
