237f2d413647446bb0e1a0ceb8ca247ed6ab036e
antranig <antranig@localhost>
2008-06-05 00:51:30 +0000

    Comment adjustment, and deprecation of UILabelTargetDecorator

--- src/uk/org/ponder/rsf/components/decorators/UILabelTargetDecorator.java
+++ src/uk/org/ponder/rsf/components/decorators/UILabelTargetDecorator.java
@@ -8,6 +8,9 @@ import uk.org.ponder.rsf.components.UIComponent;
 /** Specifies that the decorated component is a label which targets another
  * component. In HTML, this would be represented by a &lt;label for=" tag.
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * @deprecated As of RSF 0.7.3, all id relations (label for, and td/th headers) 
+ * may be mapped directly from the template (see 
+ * <a href="http://www.caret.cam.ac.uk/jira/browse/RSF-77">http://www.caret.cam.ac.uk/jira/browse/RSF-77</a>
  */
 
 public class UILabelTargetDecorator implements UIDecorator {
