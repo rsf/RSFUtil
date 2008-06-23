fcd2caf34cceb44a3ebbc63efea3cddd73556b04
Antranig Basman <amb26@ponder.org.uk>
2008-06-23 03:07:50 +0000

    Fixes required to restore I-RSFTests:   Guard for null downmap in rewriteID branch in BranchResolver   RequiredValueGuard upgraded to TargettedMessageException   Corrected broken writeDepends branch in DARApplier

--- src/uk/org/ponder/rsf/state/guards/support/RequiredValueGuard.java
+++ src/uk/org/ponder/rsf/state/guards/support/RequiredValueGuard.java
@@ -3,6 +3,9 @@
  */
 package uk.org.ponder.rsf.state.guards.support;
 
+import uk.org.ponder.messageutil.TargettedMessage;
+import uk.org.ponder.messageutil.TargettedMessageException;
+
 
 public class RequiredValueGuard {
   private String code = "value.required";
@@ -13,7 +16,7 @@ public class RequiredValueGuard {
 
   public void setValue(Object obj) {
     if (obj == null || obj.equals("")) {
-      throw new IllegalArgumentException(code);
+      throw new TargettedMessageException(new TargettedMessage(code));
     }
   }
 
