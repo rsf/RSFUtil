7f520decafd6272a694d872c4ba4357a8b41213a
antranig <antranig@localhost>
2008-09-11 20:43:28 +0000

    RSF-131: Corrected basic sitemap functionality with PolymorphicLeafObjectParser

--- test/uk/org/ponder/rsf/test/sitemap/TestSiteMap.java
+++ test/uk/org/ponder/rsf/test/sitemap/TestSiteMap.java
@@ -3,24 +3,23 @@
  */
 package uk.org.ponder.rsf.test.sitemap;
 
-import java.util.HashMap;
-
 import uk.org.ponder.rsf.bare.junit.PlainRSFTests;
 import uk.org.ponder.rsf.viewstate.EntityCentredViewParameters;
-import uk.org.ponder.rsf.viewstate.support.BasicViewParametersParser;
+import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
+import uk.org.ponder.rsf.viewstate.SiteMap;
 
 public class TestSiteMap extends PlainRSFTests {
   public TestSiteMap() {
     contributeConfigLocation("classpath:uk/org/ponder/rsf/test/sitemap/sitemap-context.xml");
   }
-  
+   
   public void testParseECVP() {
-    BasicViewParametersParser bvpp = (BasicViewParametersParser) applicationContext.getBean("viewParametersParser");
-    HashMap attrmap = new HashMap();
-    attrmap.put("flowtoken", "ec38f0");
-    EntityCentredViewParameters ecvp = (EntityCentredViewParameters) bvpp.parse(new String[] {"recipe", "3652"}, attrmap);
-    System.out.println("ECVP for entity " + ecvp.entity.ID + " of type " + ecvp.entity.entityname);
-  }
+    SiteMap map = (SiteMap) applicationContext.getBean("siteMap");
+    
+    assertEquals(EntityCentredViewParameters.class, map.view.get("recipe").getClass());
+    assertEquals(SimpleViewParameters.class, map.view.get("recipes").getClass());
+    
+ }
 
 }
 
